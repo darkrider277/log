8de77bdf511054e469458c7dcb96c0fa1e080d67
duybt <duybt@tandan.com.vn>
2020-09-10 09:41:24 +0700

    update

--- index.js
+++ index.js
@@ -2,31 +2,72 @@ const express = require('express')
 const app = express();
 const axios = require('axios')
 const bodyParser = require('body-parser')
+const multer = require('multer');
+const csv = require('csvtojson')
 app.use(bodyParser.json());       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+  limit: "50mb",
+  extended: true,
+  parameterLimit: 50000
 }));
 
-app.post('/', (req, res) => {
-  var raw = req.body.log
+global.__basedir = __dirname;
 
-  var b = raw.split('|')
+// -> Multer Upload Storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __basedir + '/file')
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname)
+  }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/', upload.single('file'), (req, res) => {
+  var statusArray = []
+  var ret = {}
+
+  csv()
+    .fromFile(__basedir + '/file/' + req.file.filename)
+    .then((obj) => {
+      
+      Promise.all(
+        obj.map(async (item, index) => {
+          ret = await handleCSV(item)
+          console.log(index)
+          statusArray.push(ret)
+        })
+      ).then(() => res.send(statusArray))
+      
+    })
+    .catch((e) => console.log(e))
+    
+})
+
+app.listen(8000, () => {
+  console.log('Example app listening on port 8000!')
+});
+
+const handleCSV = async (item) => {
+  var b = item["Raw Message"].split('|')
   var c = b[b.length - 1]
   var d = c.split('=')
 
   //handle severity
-  var exSlice = b.slice(0, b.length-1)
+  var exSlice = b.slice(0, b.length - 1)
   var severity = 0
   exSlice.map(ele => {
     switch (ele) {
       case "Low":
         severity = 1;
         break;
-    
+
       case "Medium":
         severity = 2;
         break;
-        
+
       case "High":
         severity = 3;
         break;
@@ -48,13 +89,13 @@ app.post('/', (req, res) => {
     logObject.push(arr[1])
   })
   //handling missing fields of logObject
-  var dpt_index = d.findIndex(ele => ele == " sourceTranslatedZoneURI")
-  var spt_index = d.findIndex(ele => ele == " destinationZoneURI")
+  var dpt_index = d.findIndex(ele => ele.includes(" dpt"))
+  var spt_index = d.findIndex(ele => ele.includes(" spt"))
   var dtz_index = d.findIndex(ele => ele.includes("dtz"))
 
   //handling fields available in logObject
   var timestamp = logObject.findIndex(ele => ele == "rt")
-  var category = logObject.findIndex(ele => ele == "cs1")
+  // var category = logObject.findIndex(ele => ele == "cs4")
   var action = logObject.findIndex(ele => ele == "act")
   var signature = logObject.findIndex(ele => ele == "categorySignificance")
   var dest_ip = logObject.findIndex(ele => ele == "dst")
@@ -67,15 +108,66 @@ app.post('/', (req, res) => {
   var lon = logObject.findIndex(ele => ele == "dlong")
   var ip = logObject.findIndex(ele => ele == "dvc")
 
+  /*var indexCate = logObject[category - 1].split('-')[0]
+  switch (indexCate) {
+    case 'IA':
+      category = 1;
+      break;
+
+    case 'E':
+      category = 2;
+      break;
+
+    case 'P':
+      category = 3;
+      break;
+
+    case 'PE':
+      category = 4;
+      break;
+
+    case 'DE':
+      category = 5;
+      break;
+
+    case 'CA':
+      category = 6;
+      break;
+
+    case 'D':
+      category = 7;
+      break;
+
+    case 'LM':
+      category = 8;
+      break;
+
+    case 'C':
+      category = 9;
+      break;
+
+    case 'CC':
+      category = 10;
+      break;
+
+    case 'I':
+      category = 12;
+      break;
+
+    default:
+      category = 11;
+      break;
+  }*/
+
   //handling tags and geoip
   var tags = 1
   if (
     dtz_index < 0
-    || ip < 0
-    || lat < 0
-    || lon <0
-    || logObject[lat + 1] == 0
-    || logObject[lon + 1] == 0) {
+    && ip < 0
+    && lon < 0
+    && logObject[lat + 1] == 0
+    && lat < 0
+    && logObject[lon + 1] == 0) {
     tags = 0
   }
 
@@ -110,51 +202,50 @@ app.post('/', (req, res) => {
 
   //handling json server return object 
   var json = {}
-  json.timestamp = Number(logObject[timestamp + 1] +".3590641")
+  json.timestamp = Number(logObject[timestamp + 1] + ".3590641")
   json.vendor_id = 'MISOFT_01'
   json.unit_id = 'MISOFT_01'
   json.sensor_id = '5f50a0255f627d06738587ee'
-  json.category = Number('9')
+  json.category = 1
   json.action = jsonAction
   json.signature = logObject[signature + 1]
   json.severity = Number(severity)
   json.direction = Number(logObject[direction + 1])
   json.dest_ip = logObject[dest_ip + 1]
-  json.dest_port = Number(d[dpt_index + 2].split(' ')[0])
+  json.dest_port = Number(d[dpt_index + 1].split(' ')[0])
   json.src_ip = logObject[src_ip + 1]
-  json.src_port = Number(d[spt_index + 2].split(' ')[0])
+  json.src_port = Number(d[spt_index + 1].split(' ')[0])>0? Number(d[spt_index + 1].split(' ')[0]):53
   json.proto = logObject[proto + 1]
   json.domain = logObject[domain + 1]
   json.host = "CYQPTL"
   json.tags = tags
-  if (tags==1) {
+  if (tags == 1) {
     json.geoip = {
       region_name: d[dtz_index + 1].split(' ')[0],
       timezone: d[dtz_index + 1].split(' ')[0],
       city_name: d[dtz_index + 1].split(' ')[0],
-      lat: Number(logObject[lat + 1]),
+      lat: Number(logObject[lat + 1]) > 0 ? Number(logObject[lat + 1]) : 0.1,
       country_name: d[dtz_index + 1].split(' ')[0],
-      lon: Number(logObject[lon + 1]),
+      lon: Number(logObject[lon + 1]) > 0 ? Number(logObject[lon + 1]) : 0.1,
       ip: logObject[ip + 1],
     }
   }
-  else {
-    json.geoip = null
-  }
 
-  var statusRespone 
+  var statusRespone = 0
 
-  axios.put(
+  var a = await axios.put(
     "https://api.soc.gov.vn/api/v1/alerts",
     json,
-    { headers: { "Authorization": "Basic JVWYM92QXPVBGHPP", "Content-Type": "application/json"}}
+    { headers: { "Authorization": "Basic JVWYM92QXPVBGHPP", "Content-Type": "application/json" } }
   )
-    .then(r => statusRespone = r)
-  .catch(e => console.log(e))
+    .then(r => {
+      statusRespone = r  
+      console.log(json)
+    })
+    .catch(e => {
+      statusRespone = e 
+      console.log(json)
+    })
+  return (statusRespone.data)
 
-  res.send(json)
-})
-
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
-});
+}
