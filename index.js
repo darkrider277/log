ec8b73504c226626b7f8adab970c732d44f8efe5
thotd <darkrider277@gmail.com>
2020-09-11 15:06:27 +0700

    update

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express();
 var axios = require('axios')
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
-  extended: false,
+  extended: true
 }));
+app.use(bodyParser.raw());
 
 app.listen(4000, () => {
   console.log('Example app listening on port 4000!')
